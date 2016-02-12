cbd4d3c79624005a1d576183e507f68f73b11a60
Guido Bakkes <gbakkes@gmail.com>
2016-02-12 16:58:25 +0100

    Set up click functionality for add task button.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 //Problem: User interaction doesn't provide desired results.
 //Solution: Add interactivity so the user can manage daily tasks.
 
+var taskInput = document.getElementById("new-task"); //new-task
+var addButton = document.getElementsByTagName("button")[0]; //first button
+var incompleteTasksHolder = document.getElementById("incomplete-tasks"); //incomplete-tasks
+var completedTasksHolder = document.getElementById("completed-tasks"); //completed-tasks
+
 //Add a new task
 var addTask = function() {
+    console.log("Add task...")
     //When the button is pressed
     //Create a new list item with text from #new-task:
         //input (checkbox)
@@ -15,6 +21,8 @@ var addTask = function() {
 
 //Edit an existing task
 var editTask = function() {
+    console.log("Edit task...")
+
     //When the edit button is pressed
         //if the class of the parent is .editMode
             //Switch from .editMode
@@ -28,18 +36,27 @@ var editTask = function() {
 
 //Delete an existing task
 var deleteTask = function() {
+    console.log("Delete task...")
+
     //When the delete button is pressed
         //remove the parent list item from the ul
 }
 
 //Mark a task as complete
 var taskCompleted = function() {
+    console.log("Task complete...")
+
     //When the checkbox is checked
         //append the task list item to the #completed-tasks
 }
 
 //Mark a task as incomplete
 var taskIncomplete = function() {
+    console.log("Task incomplete...")
+
     //When the checkbox is unchecked
         //append the task list item to the #incomplete-tasks
 }
+
+//Set the click handler to the addTask function
+addButton.onclick = addTask;
